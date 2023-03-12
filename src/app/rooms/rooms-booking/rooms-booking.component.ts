a5cc7c0d3dd978fa28e69457cf62aee5f2f43658
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-03-12 20:42:57 +0530

    Added rooms-booking component

---
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map, pipe } from 'rxjs';
+
+@Component({
+  selector: 'taj-rooms-booking',
+  templateUrl: './rooms-booking.component.html',
+  styleUrls: ['./rooms-booking.component.scss']
+})
+
+export class RoomsBookingComponent implements OnInit {
+
+  // by subscribing to an sevice (caused memory leak problems . . . )
+  roomId : number | string = 0;
+
+  constructor(private router : ActivatedRoute) {}
+
+  // without subscribing :
+  roomId$ = this.router.params.pipe(map((params) => params['roomId']));
+
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      this.roomId = params['roomId'];
+      console.log(this.roomId);
+    });
+  }
+}
